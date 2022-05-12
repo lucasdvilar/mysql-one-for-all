e02e7c546396871217191fb2bb0cfb5432f41577
Lucas Vilar <lucasdantasvilar@hotmail.com>
2022-05-12 14:44:32 -0300

    requirement 6

---
+++ desafio6.sql
@@ -0,0 +1,4 @@
+SELECT MIN(p.price) AS faturamento_minimo, MAX(p.price) AS faturamento_maximo, ROUND(AVG(p.price), 2) AS faturamento_medio, ROUND(SUM(p.price), 2) AS faturamento_total
+FROM SpotifyClone.plans AS p
+INNER JOIN SpotifyClone.users AS u
+ON p.id = u.plan_id;
